943c963ad45c0b5eef022abf3c54f51b8033dbce
Monika <monikahvesko@gmail.com>
2021-03-30 17:19:37 +0300

    added db class config

--- persistence/src/main/java/com/epam/esm/dao/TagDao.java
+++ persistence/src/main/java/com/epam/esm/dao/TagDao.java
@@ -1,4 +1,13 @@
 package com.epam.esm.dao;
 
+import com.epam.esm.entity.Tag;
+
+import java.util.List;
+
 public interface TagDao {
+    List<Tag> findAll();
+
+    void insert(Tag tag);
+
+    void deleteById(Long id);
 }
