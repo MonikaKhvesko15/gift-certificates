d3a7bcfbb70121177938a4334f5018eb442488aa
Monika <monikahvesko@gmail.com>
2021-04-20 18:21:23 +0300

    Revert "Merge pull request #1 from MonikaKhvesko15/module_2"
    This reverts commit a9391e66a412c8cbc61f68526970291b659d836e, reversing
    changes made to 26bb9b413fdaa420a5b59dc53981d7014bb4a542.

--- persistence/src/main/java/com/epam/esm/repository/CertificateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epam.esm.repository;
-
-import com.epam.esm.entity.Certificate;
-
-public interface CertificateRepository extends Repository<Certificate> {
-    Certificate update(Long id, Certificate certificate);
-
-    void deleteCertificateTags(Long certificateId);
-}
